91f811846b011754eb65149829322ae607951731
Benjamin Isaacson <benjamin.isaacson@gmail.com>
2023-04-05 16:58:39 -0700

    Create explode-jars.bat

---
+++ explode-jars.bat
@@ -0,0 +1,28 @@
+@REM Script to replace jar files with exploded directories
+
+@REM The jar files will be located recursively in the JARPATH directory
+@REM and all jar files will be replaced with exploded directories.
+@REM The exploded directories will be named the same as the jar files,
+@REM sans the .jar extension.
+
+@REM Requires 7-Zip
+
+
+@REM 7-Zip path
+set SEVENZIP="C:\Program Files\7-Zip\7z.exe"
+
+@REM Path to the directory containing the jar files
+set JARPATH=C:\path\to\target-dir
+
+
+@REM Find all jar files in the JARPATH directory
+for /f "delims=" %%a in ('dir /b /s /a-d %JARPATH%\*.jar') do (
+    @REM Extract the jar file to ~dpn (d=drive p=path n=name w/o extension)
+    %SEVENZIP% x %%a -y -o%%~dpna
+
+    @REM Delete the jar file
+    del %%a
+)
+
+@REM End of script
+pause
