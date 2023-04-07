c43e918dce13cfc3d16786e5b71cd241bcd4c91c
Benjamin Isaacson <benjamin.isaacson@gmail.com>
2023-04-07 11:20:04 -0700

    Add .jar to exploded directory names
    This was necessary for exploded EAR deployments in Wildfly 17.

--- explode-jars.sh
+++ explode-jars.sh
@@ -5,8 +5,10 @@ PATH_VAR="/path/to/java/application"
 
 # Find all jar files and replace them with "exploded" files
 for f in $(find $PATH_VAR -type f -name "*.jar"); do
-    # Unzip the jar file into a directory with the same name but without the .jar extension
-    unzip $f -d ${f/.jar/}
+    # Unzip the jar file into a directory with the same name with .tmp added
+    unzip $f -d $f.tmp
     # Remove the original jar file
     rm -f $f
+    # Remove the .tmp from the directory name
+    mv $f.tmp $f
 done
